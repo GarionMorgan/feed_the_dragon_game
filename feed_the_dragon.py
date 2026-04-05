a4dadd1e54e1ce89e09aeca8bd5bcf63ebc8c829
github.com/GarionMorgan <garmorgan14@yahoo.com>
2026-04-04 20:42:06 -0500

    Completed game and finished game loop

--- feed_the_dragon.py
+++ feed_the_dragon.py
@@ -98,7 +98,31 @@ while running:
     #Update HUD
     score_text = font.render("Score: " +str(score), True, GREEN, DARKGREEN)
     lives_text = font.render("Lives: " +str(player_lives), True, GREEN, DARKGREEN)
-    
+
+    #check for game over
+    if player_lives == 0:
+        display_surface.blit(game_over_text, game_over_rect)
+        display_surface.blit(continue_text, continue_rect)
+        pygame.display.update()
+
+        #pause game until player continues, then reset game
+        pygame.mixer.music.stop()
+        is_paused = True
+        while is_paused:
+            for event in pygame.event.get():
+                #player plays again
+                if event.type == pygame.KEYDOWN:
+                    score = 0
+                    player_lives = PLAYER_STARTING_LIVES
+                    player_rect.y = WINDOW_HEIGHT // 2
+                    coin_velocity = COIN_STARTING_VELOCITY
+                    pygame.mixer.music.play(-1,0.0)
+                    is_paused = False
+                #player wants to quit
+                if event.type == pygame.QUIT:
+                    is_paused = False
+                    running = False
+
     #fill display
     display_surface.fill(BLACK)
 
